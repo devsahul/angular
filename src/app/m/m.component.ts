f15eca90f5e40fd23885168ab8a3968062d99f47
SAhul CHaudhary <developersahul@SAhuls-MacBook-Air.local>
2024-01-26 23:23:27 +0530

    first angular-commit1

---
+++ src/app/m/m.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-m',
+  standalone: true,
+  imports: [],
+  templateUrl: './m.component.html',
+  styleUrl: './m.component.css'
+})
+export class MComponent {
+
+}
