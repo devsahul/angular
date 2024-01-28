68877c1ce501adfae3988c7edc7828b6441c757c
SAhul CHaudhary <developersahul@SAhuls-MacBook-Air.local>
2024-01-28 18:01:42 +0530

    “sahul-commitdev1“

---
+++ src/app/bnmhdsgdj/bnmhdsgdj.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bnmhdsgdj',
+  standalone: true,
+  imports: [],
+  templateUrl: './bnmhdsgdj.component.html',
+  styleUrl: './bnmhdsgdj.component.css'
+})
+export class BnmhdsgdjComponent {
+
+}
